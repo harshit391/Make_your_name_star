e00f3c3c9d7c589f9516e6179ffe922b21736e76
Harshit Singla <122436259+harshit391@users.noreply.github.com>
2023-06-16 10:38:52 +0530

    Add files via upload

--- Alphabet_Star_Pattern/Q.java
+++ Alphabet_Star_Pattern/Q.java
@@ -11,32 +11,49 @@ public class Q {
 
         int i,j;
 
-        for (i=0;i<x*2-1;i++)
+        for (i=0;i<x*2-2;i++)
         {
-            if (i==0 || i==x*2-2) {
-                for (j=0;j<x;j++) 
+            if (i==0) {
+                for (j=0;j<x-1;j++) 
                 {
-                    System.out.print("* ");
+                    if (j==x-1 || j==0)
+                    System.out.print("  ");
+
+                    else System.out.print("* ");
                 }
                 System.out.println();
-
-                
             }
 
-        
-
-            else 
-            {   
+            else if (i>(x-x/2+1) && i<(x+2)){
                 for (j=0;j<x;j++) 
                 {
-                    if (j==0 || j==x-1) {
+                    if (j==0 || j==x-1 || j==(i-x/2))
+                    System.out.print("* ");
 
-                        System.out.print("* ");
+                    else System.out.print("  ");
+                }
+                System.out.println();
+            }
 
-                    }
+            else if(i==x*2-3) {
+                for (j=0;j<x+1;j++) 
+                {
+                    if (j==x-1 || j==0)
+                    System.out.print("  ");
 
-                    if (i>=x+1 && i<=x+3) System.out.print("* ");
+                    else if (j==x) System.out.print("* ");
 
+                    else System.out.print("* ");
+                }
+                System.out.println();
+            }
+            
+
+            else 
+            {
+                for (j=0;j<x;j++) 
+                {
+                    if (j==0 || j==x-1) System.out.print("* ");
                     else System.out.print("  ");
                 }
                 System.out.println();
